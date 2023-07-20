23399e7db74d717673763325976a1983337fb550
ZouYuhua <zouy@vmware.com>
2023-07-20 11:29:04 +0800

    support freebsd
    Signed-off-by: ZouYuhua <zouy@vmware.com>

--- linux/vhba_hot_add_remove/run_iozone_test.sh
+++ linux/vhba_hot_add_remove/run_iozone_test.sh
@@ -62,7 +62,7 @@ function run_iozone()
 
     iozone_file="$testdir_path/iozone.csv"
     # Get partition size
-    if [[ "$os_distribution" ~= FreeBSD ]]; then
+    if [[ "$os_distribution" =~ FreeBSD ]]; then
         part_size=`gpart list ${dev_name} | head -n 15 | grep Mediasize | awk '{print $3}'`
     else
         part_size=`lsblk -o NAME,SIZE,TYPE | grep -i part | grep -i ${part_name} | awk '{print $2}'`
