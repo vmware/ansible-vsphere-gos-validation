15df869eb06032855f5128fbb432a0841f5c2c79
Qi (Keira) Zhang <keirazhang@users.noreply.github.com>
2024-05-24 14:02:25 +0800

    [Linux] Support to test installing open-vm-tools from source on FreeBSD (#582)
    Signed-off-by: Qi Zhang <qi-keira.zhang@broadcom.com>

--- linux/open_vm_tools/ovt_installer.sh
+++ linux/open_vm_tools/ovt_installer.sh
@@ -30,12 +30,37 @@ commands=("autoreconf -f -i"
           "ldconfig")
 rc=0
 
+system=$(uname -s)
+if [ "$system" == "FreeBSD" ]; then
+    # Create the directory path of tools.conf on FreeBSD
+    mkdir -p $INSTALL_PREFIX/share/vmware-tools/
+    chmod a+rx $INSTALL_PREFIX/share/vmware-tools/
+
+    # Workaround for compiliing error on FreeBSD 14
+    freebsd_version=$(freebsd-version | cut -d '.' -f 1)
+    echo "FreeBSD release version is $freebsd_version"
+    if [ $freebsd_version -ge 14 ]; then
+        vmmemctl_patch="/usr/ports/emulators/open-vm-tools/files/patch-modules_freebsd_vmmemctl_os.c"
+        vmblock_patch="/usr/ports/emulators/open-vm-tools/files/patch-modules_freebsd_vmblock_vfsops.c"
+
+        if [ -e "$vmmemctl_patch" ]; then
+            echo "Applying patch $vmmemctl_patch"
+            patch < $vmmemctl_patch
+        fi
+
+        if [ -e "$vmblock_patch" ]; then
+            echo "Applying patch $vmblock_patch"
+            patch < $vmblock_patch
+        fi
+    fi
+fi
+
 for cmd in  "${commands[@]}"; do
     echo ">>>Beginning of command: $cmd<<<"
     eval "$cmd"
     rc=$?
     if [ $rc -ne 0 ]; then
-        echo "Failed to execute command: $cmd"
+        echo "Failed to execute command: $cmd" >&2
         exit $rc;
     fi
     echo ">>>End of command: $cmd<<<"
