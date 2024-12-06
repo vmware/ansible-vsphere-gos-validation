42bb49fd0c75833deb47e1d797a3535582c668d8
Qi Zhang <qi-keira.zhang@broadcom.com>
2024-12-06 14:24:08 +0800

    Set deploy_vm case name when new_vm=true
    Signed-off-by: Qi Zhang <qi-keira.zhang@broadcom.com>

--- plugin/ansible_vsphere_gosv_log.py
+++ plugin/ansible_vsphere_gosv_log.py
@@ -780,7 +780,8 @@ class CallbackModule(CallbackBase):
         test_idx = 0
         for test_id in self.test_runs:
             test_result = self.test_runs[test_id]
-            if test_result.name == 'deploy_vm':
+            if (str(self.testing_vars.get('new_vm', False)).lower() == 'true' and
+                test_result.name == 'deploy_vm'):
                 # Update deploy_vm test case name
                 if self.testing_vars.get('vm_deploy_method', '').lower() == 'ova':
                     if self.testing_testcase_file and 'windows' in self.testing_testcase_file:
