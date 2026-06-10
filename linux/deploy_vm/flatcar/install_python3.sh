6ab9e026aaff56b34db080621f6da9a58e203173
ZouYuhua <yuhua.zou@broadcom.com>
2026-06-10 13:50:52 +0800

    No python interpreters found for Flatcar 4081.3.8
    Signed-off-by: ZouYuhua <yuhua.zou@broadcom.com>

--- linux/deploy_vm/flatcar/install_python3.sh
+++ linux/deploy_vm/flatcar/install_python3.sh
@@ -79,6 +79,13 @@ else
     rm -rf $ap_python3_download_dir
 
     echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:$ap_python3_bin_dir" >>/etc/environment
+    if [ $? -eq 0 ]; then
+        echo "Successfully updated /etc/environment with Python path."
+    else
+        echo "Error: Failed to write to /etc/environment"
+        exit 1
+    fi
+
     python_version=$($ap_python3_bin_dir/python -V)
     rc=$?
     if [ $rc -eq 0 ]; then
