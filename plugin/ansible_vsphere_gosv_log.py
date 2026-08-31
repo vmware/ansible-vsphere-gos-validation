585418f6089801c2df9e4f1001be53c3b70cfb08
yh012629 <yanhui.he@broadcom.com>
2026-08-31 17:30:01 +0800

    Fix GOSV-6542: Add SLES Operating Mode to VM detail info in test summary log
    Signed-off-by: yh012629 <yanhui.he@broadcom.com>

--- plugin/ansible_vsphere_gosv_log.py
+++ plugin/ansible_vsphere_gosv_log.py
@@ -262,6 +262,10 @@ class VmDetailInfo(VmGuestInfo):
 
             self.Guest_OS_Distribution = ansible_gosv_facts.get('vm_guest_os_distribution', '')
 
+            sles_mode = ansible_gosv_facts.get('sles_operating_mode', '')
+            if sles_mode:
+                self.SLES_Operating_Mode = sles_mode.capitalize()
+
             if 'CloudImage' in self.Guest_OS_Distribution:
                 self.Cloud_Image_Build = (ansible_gosv_facts.get('cloud_image_build_name', '') + " (serial: "
                                           + ansible_gosv_facts.get('cloud_image_build_serial', '') + ")")
