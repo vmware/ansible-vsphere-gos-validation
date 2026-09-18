1925fa456d40d4f6d6dc69201d318c67f79ecd8c
yh012629 <yanhui.he@broadcom.com>
2026-09-18 16:49:19 +0800

    Fix GOSV-6542: Address code review feedback for SLES Immutable mode support and package discovery
    Signed-off-by: yh012629 <yanhui.he@broadcom.com>

--- plugin/ansible_vsphere_gosv_log.py
+++ plugin/ansible_vsphere_gosv_log.py
@@ -262,9 +262,10 @@ class VmDetailInfo(VmGuestInfo):
 
             self.Guest_OS_Distribution = ansible_gosv_facts.get('vm_guest_os_distribution', '')
 
-            sles_mode = ansible_gosv_facts.get('sles_operating_mode', '')
-            if sles_mode:
-                self.SLES_Operating_Mode = sles_mode.capitalize()
+            if 'is_sles_immutable' in ansible_gosv_facts:
+                self.SLES_Operating_Mode = 'Immutable' if ansible_gosv_facts.get('is_sles_immutable') else 'Standard'
+            elif ansible_gosv_facts.get('sles_operating_mode'):
+                self.SLES_Operating_Mode = ansible_gosv_facts.get('sles_operating_mode').capitalize()
 
             if 'CloudImage' in self.Guest_OS_Distribution:
                 self.Cloud_Image_Build = (ansible_gosv_facts.get('cloud_image_build_name', '') + " (serial: "
